3ceee85c112605d01a7c3642fa304e80801c0301
WillyChen <willy700628@gmail.com>
2013-06-16 21:18:11 +0800

    Revert "Revert "Revert "Part2"""
    This reverts commit 56a21e687743200db7024578a1c7e9bea1915f18.

---
+++ BabyAppease/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BabyAppease
+//
+//  Created by Willy on 13/6/13.
+//  Copyright (c) 2013年 tw.idv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
