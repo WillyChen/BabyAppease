1bcd955b39325c160d64814d3aff9bd125785478
WillyChen <willy700628@gmail.com>
2013-06-16 21:19:51 +0800

    Revert "Revert "Revert "Revert "Revert "Part2"""""
    This reverts commit 2d9b5b384a3a7f7001f78729cf5bc1408e209192.

---
+++ BabyAppease/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BabyAppease
+//
+//  Created by Willy on 13/6/13.
+//  Copyright (c) 2013年 tw.idv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
